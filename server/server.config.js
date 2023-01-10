3f12cc2de924f7191b88f48d9e1d5b8491801e42
lovexlc <106605081+lovexlc@users.noreply.github.com>
2023-01-10 09:21:57 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://yourdomain'
+const BAREURL = 'https://hideip.lovexl.engineer'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
